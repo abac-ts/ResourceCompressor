a34e99934f328bb73b4e8f6f49180b8dd8507dda
Ryan Holdren <ryan.c.holdren@gmail.com>
2016-01-20 21:34:42 -0800

    Changed package name and flushed out the pom.xml as required by Sonatype.

--- README.md
+++ README.md
@@ -1,31 +1,24 @@
-# em-maven-resource-optimizer
+# ResourceGZipper
 
 Simple Maven plugin to gzip JS & CSS files at build time. No other dependencies, other than Maven itself.
 
-## Usage: 
-	<plugin>
-		<groupId>com.ekaminds</groupId>
-		<artifactId>em-maven-resource-optimizer</artifactId>
-		<version>0.0.1</version>
-		<executions>
-			<execution>
-				<id>js-gzip</id>
-				<phase>process-classes</phase>
-				<goals>
-					<goal>gzip</goal>
-				</goals>
-				<configuration>
-					<resourcedir>${project.basedir}/src/main/webapp/js-min</resourcedir>
-				</configuration>
-			</execution>
-		</executions>
-	</plugin>
-
-## TODO
- * Much better implementation. *I know this can be written much better. I've written just to serve my purpose of gzip wro4j minified JS & CSS files at build time.*
- * I m using this in production with Java 8 from last 6 months without any issue. I've not tested on Java 6/7 though.
- * Publish to Maven central
- 
+## Usage:
+<plugin>
+	<groupId>com.github.ryanholdren</groupId>
+	<artifactId>resourcegzipper</artifactId>
+	<version>0.0.1</version>
+	<executions>
+		<execution>
+			<id>gzip-css</id>
+			<goals>
+				<goal>gzip</goal>
+			</goals>
+			<configuration>
+				<resourcedir>${project.build.directory}/${project.build.finalName}/css</resourcedir>
+			</configuration>
+		</execution>
+	</executions>
+</plugin>
 
 ## License
 Licensed under the MIT license.
--- src/main/java/com/github/ryanholdren/resourcegzipper/ResourceGZipperMojo.java
+++ src/main/java/com/github/ryanholdren/resourcegzipper/ResourceGZipperMojo.java
@@ -1,4 +1,4 @@
-package com.manikanta.optimizer.maven;
+package com.github.ryanholdren.resourcegzipper;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.zip.GZIPOutputStream;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
@@ -17,7 +16,8 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * Maven plugin to gzip the JS &amp; CSS files.
- * <p>Usage: in pom.xml,
+ * <p>
+ * Usage: in pom.xml,
  * <pre>
  * <!-- GZIP the minified files -->
  *&lt;plugin&gt;
@@ -37,104 +37,105 @@ import org.apache.maven.plugins.annotations.Parameter;
  *		&lt;/execution&gt;
  *	&lt;/executions&gt;
  *&lt;/plugin&gt;
- *</pre>
+ * </pre>
  *
  * @author Manikanta G
  */
 @Mojo(name = "gzip", defaultPhase = LifecyclePhase.PROCESS_CLASSES, threadSafe = true)
-public class GZIPMojo extends AbstractMojo {
+public class ResourceGZipperMojo extends AbstractMojo {
 
 	/**
 	 * Location of the resource directories.
+	 *
 	 * @required
 	 */
 	@Parameter(property = "resourcedir", required = true)
 	private File resourcedir;
-	
-	private Log LOG;
-	
+
+	private Log log;
+
 	public void execute() throws MojoExecutionException {
-		if(resourcedir == null) {
-			LOG.error("resourcedir is null or empty");
+		if (resourcedir == null) {
+			log.error("resourcedir is null or empty");
 			throw new MojoExecutionException("resourcedir is not used.");
 		}
-		
-		if(! resourcedir.exists()) {
+
+		if (!resourcedir.exists()) {
 			throw new MojoExecutionException("resourcedir '" + resourcedir + "' doesn't exist");
 		}
-		
-		if(! resourcedir.isDirectory()) {
+
+		if (!resourcedir.isDirectory()) {
 			throw new MojoExecutionException("resourcedir '" + resourcedir + "' is not a directory");
 		}
-		
+
 		FilenameFilter resourceFilenameFilter = new FilenameFilter() {
 			@Override
 			public boolean accept(File dir, String name) {
 				return name.endsWith(".js") || name.endsWith(".css");
 			}
 		};
-		
+
 		gzipFiles(resourcedir, resourceFilenameFilter);
-		
+
 	}
-	
+
 	private void gzipFiles(File resourceDir, FilenameFilter resourceFilenameFilter) {
 		File[] resourceFiles = resourceDir.listFiles(resourceFilenameFilter);
-		
-		if(resourceFiles.length == 0) {
-			LOG.info(resourceDir + " doesn't have any resource files; skipping...");
+
+		if (resourceFiles.length == 0) {
+			log.info(resourceDir + " doesn't have any resource files; skipping...");
 			return;
 		}
-		
-		for(File resourceFile : resourceFiles) {
-			LOG.info("gzipping " + resourceFile);
+
+		for (File resourceFile : resourceFiles) {
+			log.info("gzipping " + resourceFile);
 			gzipFile(resourceFile);
 		}
-		
+
 	}
-	
+
 	private void gzipFile(File sourceFile) {
 		BufferedInputStream bis = null;
 		GZIPOutputStream gzipOut = null;
 		try {
 			bis = new BufferedInputStream(new FileInputStream(sourceFile));
-			
+
 			File gzippedFile = new File(sourceFile.getAbsolutePath() + ".gz");
-			if(gzippedFile.exists()) {
+			if (gzippedFile.exists()) {
 				gzippedFile.delete();
 			}
 			gzippedFile.createNewFile();
-			
+
 			gzipOut = new GZIPOutputStream(new FileOutputStream(gzippedFile));
-			
+
 			for (int ch = bis.read(); ch != -1; ch = bis.read()) {
 				gzipOut.write(ch);
 			}
-			
+
 			gzipOut.flush();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			if(bis != null) {
+			if (bis != null) {
 				try {
 					bis.close();
 				} catch (IOException e) {
-					LOG.error("Error while closing the input stream");
+					log.error("Error while closing the input stream");
 				}
 			}
-			if(gzipOut != null) {
+			if (gzipOut != null) {
 				try {
 					gzipOut.close();
 				} catch (IOException e) {
-					LOG.error("Error while closing the gzipped output stream");
+					log.error("Error while closing the gzipped output stream");
 				}
 			}
 		}
 	}
-	
+
 	public void setLog(Log log) {
-		this.LOG = log;
+		this.log = log;
 	}
 
 }
